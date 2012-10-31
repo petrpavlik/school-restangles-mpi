a348d448f52c17147db9b16c9027ba9b2c2e10f2
Petr Pavlik <pavlipe7@gmail.com>
2012-10-31 14:08:35 +0100

    Initial Commit

---
+++ POOParaler/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  POOParaler
+//
+//  Created by Petr Pavlik on 10/31/12.
+//  Copyright (c) 2012 Petr Pavlik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
